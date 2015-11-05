9fe4db237f1589a0dca38366a39406b4988e01c9
jim Andresen <jim@wilshirelawfirm.com>
2015-11-05 08:44:55 -0800

    Update home page

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -18,7 +18,10 @@
         
             <h2 class="content-subhead">Personal Injury Attorney</h2>
             <p>
-				Renowned personal injury trial attorney Colin Jones is dedicated to protecting the interests of injury victims throughout California. He has extensive experience handling a wide variety of serious cases, including brain injury, spinal cord injury, amputation, and wrongful death. He is as caring and compassionate with his clients as he is tenacious and aggressive with his opponents. A firm believer in justice, Colin represents only those who have been wronged, and uses his exceptional courtroom skills and strong sense of empathy to guide jurors towards truth so they can do right by victims. 
+				Renowned personal injury trial attorney Colin Jones is dedicated to protecting the interests of injury victims throughout California. 
+            </p>
+            <p>
+                Colin has dedicated his legal career to the "little guy",  fighting insurance companies and corporations who try to duck responsibility by paying injured victims pennies on the dollar. Many attorneys "talk the talk" and  claim they are trial lawyers, but few "walk the walk" and take cases to jury verdict. When it’s time for battle, Colin is the trial warrior they send their cases to. With positive verdicts in three different states, Colin attributes his success to discovering his client's story and being able to share that with a jury.
             </p>
             <p>
             	For Colin, it is important to show the jurors that every client he represents, the compensation he is seeking is from stingy insurance companies, and not helpless individuals. By clearly communicating that compensation is being sought from monolithic corporations, he finds great success in acquiring compensation in line with the pain, suffering, personal injuries and damages to personal property, that his client's endured. 
